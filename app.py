cd48f571b30ce66b65117945e0461664cd7629ff
s4muc4 <eng.comp.samuel@hotmail.com>
2021-06-07 23:18:29 -0300

    removendo arquivos e adicionando rota principal

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
+""" @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello from Python!" """
+
+@app.route("/", methods=["GET"])
+def helloWorld():
+	return {"message":"Hospedagem no Herokuu 😎"}
 
 @app.route("/teste")
 def teste():
