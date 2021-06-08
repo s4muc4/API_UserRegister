0769b854637e1550388f0565fb6b538177ef782f
s4muc4 <eng.comp.samuel@hotmail.com>
2021-06-07 23:08:37 -0300

    removendo arquivos e adicionando rota principal

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello from Python!"
 
+@app.route("/teste")
+def hello():
+    return "Hello from Python!"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
